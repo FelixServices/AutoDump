19e4c30cf549ec3fe98596a263a48964fdc548cb
OfficialFelixVPN <96802993+OfficialFelixVPN@users.noreply.github.com>
2021-12-29 03:30:13 +0000

    Update mitigated.py

--- AutoDump/mitigated.py
+++ AutoDump/mitigated.py
@@ -7,7 +7,7 @@ embed = Embed(
         )
 
 embed.set_thumbnail('https://www.seekpng.com/png/full/205-2059350_safe-black-and-white-verified.png')
-embed.set_footer(text="Attack Detected")
+embed.set_footer(text="Attack Mitigated")
 
 hook = Webhook('https://discord.com/api/webhooks/894757651223822347/a2TRRca5ylOHxoO0squg7PVXd22wEz-8uHm8wKJ1MO8YUHf174a8f09TuOHFzn71I2tm')
 
